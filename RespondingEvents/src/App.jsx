54cc8439433ccaa09dd72e5f767f3d20a1f22425
devanidarshan <devanidarshan333@gmail.com>
2023-10-12 10:23:29 +0530

    events

---
+++ RespondingEvents/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+// import ClickOn from './Events/ClickOn'
+// import ClickOn2 from './Events/ClickOn2'
+// import ClickOn3 from './Events/ClickOn3'
+// import ClickOn4 from './Events/ClickOn4'
+// import ClickOn5 from './Events/ClickOn5'
+// import ClickOn6 from './Events/ClickOn6'
+
+function App() {
+
+  return (
+    <>
+      {/* <ClickOn/> */}
+      {/* <ClickOn2/> */}
+      {/* <ClickOn3/> */}
+      {/* <ClickOn4/> */}
+      {/* <ClickOn5/> */}
+      {/* <ClickOn6/> */}
+    </>
+  )
+}
+
+export default App
---
+++ RespondingEvents/src/Events/ClickOn4.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ClickOn4 = () => {
+
+      /* Passing a Function */
+     /* Calling a Function*/
+     
+     const handleClick = () => {
+        alert("You are use Function")
+   }
+
+  return (
+    <div>
+       <button className="button" onClick={handleClick()}>Click</button>
+    </div>
+  )
+}
+
+export default ClickOn4
