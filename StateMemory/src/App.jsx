b9ed3cd58071f545783a98477bc1eed78cffdb39
devanidarshan <devanidarshan333@gmail.com>
2023-10-14 09:33:16 +0530

    Statememory

---
+++ StateMemory/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import States from './StateMemory/States'
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <States/>
+     </div>
+    </>
+  )
+}
+
+export default App
