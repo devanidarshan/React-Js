6aa7cc473dc4e9faa84950b9c2492db64d69c2be
devanidarshan <devanidarshan333@gmail.com>
2023-10-28 10:02:32 +0530

    Usecontext

---
+++ Hook/src/Use-Context/UsecontextA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UsecontextB from './UsecontextB'
+
+const UsecontextA = () => {
+  return (
+    <div>
+    <UsecontextB/>
+    </div>
+  )
+}
+
+export default UsecontextA
