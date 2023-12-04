de9d05dfc6ca6741084fce0e82932a65328bf18f
devanidarshan <devanidarshan333@gmail.com>
2023-12-04 16:10:40 +0530

    useCallbacktask

---
+++ Hook/src/Use-Callback-Task/Task.jsx
@@ -0,0 +1,16 @@
+import React, {memo} from 'react'
+
+const Task = ({ tasks, addTask }) => {
+    console.log("child rendered");
+    return (
+        <div>
+            <h2>Tasks list</h2>
+            {tasks.map((task, i) => (
+                <p key={i}>{task}</p>
+            ))}
+            <button onClick={addTask}>Add Task</button>
+        </div>
+    )
+}
+
+export default memo(Task)
