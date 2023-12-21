694874d31fe18d37549f6f7387f25d386213c638
devanidarshan <devanidarshan333@gmail.com>
2023-12-21 08:49:24 +0530

    React-Redux

---
+++ React-Redux/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import CakeContainer from './Components/Redux/Cake/CakeContainer'
+
+function App() {
+
+  return (
+    <>
+     <CakeContainer/>
+    </>
+  )
+}
+
+export default App
