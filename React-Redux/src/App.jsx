d83610d7963bc76bb92d73ea48a92c27eee29463
devanidarshan <devanidarshan333@gmail.com>
2023-12-21 15:57:49 +0530

    React-Redux

--- React-Redux/src/App.jsx
+++ React-Redux/src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css'
-import CakeContainer from './Components/Redux/Cake/CakeContainer'
+import "./App.css";
+import CakeContainer from "./Components/CakeContainer";
+// import HooksCakeContainer from "./Components/Redux/Cake/HookCakeContainer";
+import { Provider } from "react-redux";
+import store from "./Components/Redux/Store";
 
 function App() {
-
   return (
     <>
-     <CakeContainer/>
+      <Provider store={store}>
+        <CakeContainer />
+        {/* <HooksCakeContainer/> */}
+      </Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ React-Redux/src/Components/Redux/Cake/Action.jsx
@@ -0,0 +1,8 @@
+import { BUY_CAKE } from "./Type";
+
+export default function buyCake(){
+    return{
+         type:BUY_CAKE,
+         
+    }
+}
