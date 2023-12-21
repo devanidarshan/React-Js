4f4e35cf3bd2f384e6ce7e694e6ab32c7b0ee8af
devanidarshan <devanidarshan333@gmail.com>
2023-12-21 09:37:02 +0530

    Redux-Practice

--- Redux/Task1.js
+++ Redux/Task1.js
@@ -43,5 +43,4 @@ console.log('Initial State' , store.getState());
 const unsubscribe = store.subscribe(() => console.log('Updated state' , store.getState()))
 store.dispatch(buyCake())
 store.dispatch(buyPastry())
-store.dispatch(buyPastry())
 unsubscribe()
